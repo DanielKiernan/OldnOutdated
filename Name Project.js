a75bea5c2147fe99e61b58de30e8e3d4d4659ee2
DanielKiernan <DanielKiernanJr@gmail.com>
2016-05-23 16:29:39 -0500

    Update Name Project.js

--- Name Project.js
+++ Name Project.js
@@ -1,29 +1,39 @@
-var name = (prompt("What is your name?")).toLowerCase();
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 function name() {
+	
+	var name = (prompt("What is your name?")).toLowerCase();
+	
 	switch ( name ) {
 		case "dylan":
 			alert("ur gay");
 			break;
+			
 		case "daniel":
 			alert("You are awesome");
 			break;
+			
 		case "brianna":
 			alert("You are a wonderful dancer!");
 			break;
+			
 		case "brooklynn":
 			alert("You are a baby");
 			break;
+			
 		case "mitch":
 			alert("You are a pedophile");
 			break;
+			
 		case "debra":
 			alert("You are a child abuser");
 			break;
+			
 		case "shrek":
 			alert("No it is not");
+			break;
+			
 		default:
 			alert("Your name is " + capitalizeFirstLetter(name));
 	}
