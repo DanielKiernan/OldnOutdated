7dc5d88a258ed4fab37ed21f4ff2fe7304dc6195
DanielKiernan <DanielKiernanJr@gmail.com>
2016-08-08 18:09:24 -0500

    lobby

---
+++ lobby.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		
+		<link rel="stylesheet" type="text/css" href="lobby.css">
+		<title>Click on the button</title>
+		
+	</head>
+	<body>
+	
+	  <h1>Welcome to the Lobby!</h1>
+	  
+	</body>
+</html>
